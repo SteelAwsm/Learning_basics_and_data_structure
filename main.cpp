4845be2a73427ad47f7d1d2e87e37d38cebf8102
Hemakesha R. H <78362238+SteelAwsm@users.noreply.github.com>
2022-08-08 23:54:30 +0700

    soal 1-2, no 3 can use sorting algo

--- main.cpp
+++ main.cpp
@@ -2,63 +2,29 @@
 
 using namespace std;
 
-//Soal1
-/*
-int main()
-{
-    int a;
-    printf ("Program Dimulai \ninput '0' untuk keluar\n");
-    while(true)
-    {
-        printf ("input = ");
-        scanf("%d",&a);
-        if (a == 0)
-        {
-            break;
-        }
-        else if (a%2 == 0)
-        {
-            printf("Genap\n");
-        }
-        else if (a%2 != 0)
-        {
-            printf("Ganjil\n");
-        }
-    }
-    printf ("Program Selesai");
-    return 0;
-}
-*/
 
-//Soal2
-/*
+/* Soal 1
+int factorial(int n)
+{
+    if (n == 0) return 1;       // base case
+    return (n * factorial(n-1)); // recursive case
+}
+
 int main()
 {
-    int a;
-    int i;
+    int n;
     printf ("Program Dimulai \ninput '0' untuk keluar\n");
     while(true)
     {
         printf ("input = ");
-        scanf("%d",&a);
-        if (a == 0)
+        scanf("%d",&n);
+        if (n == 0)
         {
             break;
         }
         else
         {
-            for (i=1;i<=a;i++)
-            {
-             if (i%2 != 0)
-             {
-                printf("%d ", i);
-             }
-            else if (i%2 == 0)
-             {
-                printf("* ");
-             }
-            }
-            printf ("\n");
+            printf("%d\n", factorial(n));
         }
     }
     printf ("Program Selesai");
@@ -66,83 +32,32 @@ int main()
 }
 */
 
-//Soal3
-/*
+/* Soal 2
+int numpattern(int n)
+{
+    if (n == 0) return 0;       // base case
+    else if (n==1) return 1;
+    return (n*n+numpattern(n-1)); // recursive case //Thanks Naufal for helping me bruh
+}
+
 int main()
 {
-    int total_num;
-    int i=0;
-    int m=0;
-    int j=0;
-    int flag=0;
+    int n;
     printf ("Program Dimulai \ninput '0' untuk keluar\n");
     while(true)
     {
         printf ("input = ");
-        scanf("%d",&total_num);
-        if (total_num == 0)
+        scanf("%d",&n);
+        if (n == 0)
         {
             break;
         }
         else
         {
-            for (i=1;i<=total_num;i++)
-            {
-                flag=0;
-                m=i/2;
-                for(j=2;j<=m;j++)
-                {
-                    if(i%j == 0)
-                    {
-                        printf("%d ", i);
-                        flag=1;
-                        break;
-                    }
-                }
-                if(flag==0)
-                printf("* ");
-            }
+            printf("%d\n", numpattern(n));
         }
-            printf ("\n");
     }
     printf ("Program Selesai");
     return 0;
 }
 */
-
-//Soal 4
-/*
-int main()
-{
-  int n;
-  int i;
-  int j;
-  printf ("Program Dimulai \ninput '0' untuk keluar\n");
-  while(true)
-  {
-      printf ("input =\n");
-      scanf ("%d", &n);
-      if (n == 0)
-      {
-          break;
-      }
-      else
-      {
-          int arr[n];
-          for(i=0;i<n;i++)
-          {
-              scanf("%d", &arr[i]);
-          }
-          for(j=n-1;j>=0;j--)
-          {
-              printf("%d\n", arr[j]);
-          }
-      }
-      printf ("\n");
-  }
-  printf ("Program Selesai");
-  return 0;
-}
-*/
-
-
