af65d8eb7247e21421bf703da425db9770072018
Hemakesha R. H <78362238+SteelAwsm@users.noreply.github.com>
2022-08-10 00:38:52 +0700

    adding no 3

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdio.h>
+#include <math.h>
 
 using namespace std;
 
@@ -55,6 +57,95 @@ int main()
         else
         {
             printf("%d\n", numpattern(n));
+        }
+    }
+    printf ("Program Selesai");
+    return 0;
+}
+*/
+
+/* Soal 3
+void maxmin(int arr[], int a)
+{
+    int flag = 0;
+    int flag1 = 0;
+
+    for (int i = 0; i < a; i++)
+    {
+        if (flag == 1)
+        {
+            break;
+        }
+        else
+        {
+            for (int j = 0; j < a; j++)
+            {
+                if (arr[j] > arr[i])
+                {
+                    break;
+                }
+                else if (j+1 == a)
+                {
+                    printf ("max: %d\n", arr[i]);
+                    flag = 1;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+    for (int k = 0; k < a; k++)
+    {
+        if (flag1 == 1)
+        {
+            break;
+        }
+        else
+        {
+            for (int l = 0; l < a; l++)
+            {
+                if (arr[k] > arr[l])
+                {
+                    break;
+                }
+                else if (l+1 == a)
+                {
+                    printf ("min: %d\n", arr[k]);
+                    flag1 = 1;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    int a;
+    printf ("Program Dimulai \ninput '0' untuk keluar\n");
+    while(true)
+    {
+        printf ("Jumlah elemen array = ");
+        scanf("%d",&a);
+        if (a == 0)
+        {
+            break;
+        }
+        else
+        {
+            int arr[a];
+            for (int i = 0; i < a; i++)
+            {
+                scanf ("%d", &arr[i]);
+            }
+            maxmin(arr, a);
         }
     }
     printf ("Program Selesai");
